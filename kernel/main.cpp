f32e9bbade999ca63269fcd9db4387e6ba35dd7d
tanidaiz <tanidaig@yahoo.co.jp>
2020-12-11 16:51:55 +0900

    animation

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -3,8 +3,13 @@
 extern "C" void KernelMain(uint64_t frame_buffer_base,
                            uint64_t frame_buffer_size) {
   uint8_t* frame_buffer = reinterpret_cast<uint8_t*>(frame_buffer_base);
-  for (uint64_t i = 0; i < frame_buffer_size; ++i) {
-    frame_buffer[i] = i % 256;
+  uint8_t time = 0;
+  while(1){
+    for (uint64_t i = 0; i < frame_buffer_size; ++i) {
+      frame_buffer[i] = (uint8_t)(i+time) % 256;
+    }
+    time++;
   }
+  
   while (1) __asm__("hlt");
 }
