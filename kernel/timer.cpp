bfee2ad4fb0567fb7b64bde57467c09aefe1f214
tanidaiz <tanidaig@yahoo.co.jp>
2020-12-25 14:48:19 +0900

    change frequency

--- kernel/timer.cpp
+++ kernel/timer.cpp
@@ -3,7 +3,7 @@
 #include "interrupt.hpp"
 
 namespace {
-  const uint32_t kCountMax = 0xffffffffu;
+  const uint32_t kCountMax = 0x0fffffffu;
   volatile uint32_t& lvt_timer = *reinterpret_cast<uint32_t*>(0xfee00320);
   volatile uint32_t& initial_count = *reinterpret_cast<uint32_t*>(0xfee00380);
   volatile uint32_t& current_count = *reinterpret_cast<uint32_t*>(0xfee00390);
